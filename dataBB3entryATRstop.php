69f0a211301f891f7ca69af26f2c2f4d3c135a8c
Upitdc Prado <upitdc.prado@gmail.com>
2016-10-11 17:18:02 +0800

    + Created prototype code for BB3 Entry/ATR Stop data generation that is based from SMA Entry/ATR Stop

---
+++ dataBB3entryATRstop.php
@@ -0,0 +1,88 @@
+<?php 
+	require_once('codesword_trade_signals.php');
+	require_once('dataSMA.php');
+	require_once('dataTR.php');
+
+	// returns BB3 Buy Signal and ATR based Sell/Stop Signal
+	//		{(timestamp, bb3), (timestamp, signal)}
+	function getBB3entryATRstop ($company, $from="1900-01-01 00:00:00", $to=null, 
+					$dataorg="json", $samplePeriod=15, $enSignals=false,
+					$enJsonEncode="false", $enProfitComputation=true,
+					$host, $db, $user, $pass) {
+
+		// TODO: Get OHLC data
+		$intervalPeriod = $samplePeriod * 1.5;
+		//from date has to be adjusted for the bollinger bands
+		$date = date_create($from);
+		date_add($date,date_interval_create_from_date_string("-$intervalPeriod days"));
+		$fromAdjusted =  date_format($date,"Y-m-d");
+
+		$dataOhlc = [];
+
+		//OHLC data format [timestamp,open,high,low,close,volume]
+		if ($dataorg == "highchart") {
+			$dataOhlc = getOHLC ($company, $fromAdjusted, $to, "array2", $host, $db, $user, $pass);
+		} else {
+			$dataOhlc = getOHLC ($company, $fromAdjusted, $to, "array", $host, $db, $user, $pass);
+		}
+
+		//Return if $dataOhlc is null
+		if ( ($dataOhlc == []) || ($dataOhlc == 0) ) {
+			return 0;
+		}
+
+		//TODO:Tailor data for the BB3 input
+		//Input for BB3 functions should be [timestamp,...]
+/*		$ctr = 0;
+		$dataOhlcForSMA = [];
+		foreach ((array)$dataOhlc as $ohlc) {
+			$dataOhlcForSMA[$ctr][0] = $ohlc[0];	//timestamp
+			$dataOhlcForSMA[$ctr++][1] = $ohlc[4];	//close
+		}*/
+
+		//TODO: Get BB3 Signal
+		/*$sma = codesword_sma($dataOhlcForSMA, $samplePeriod);*/
+
+		//TODO: Get BB3 Buy Signals
+/*		$smaBuy = [];
+		$ctrBuy = 0;
+		$buysellSignals = codesword_smaBuySellSignal($dataOhlcForSMA, $sma);
+		foreach ($buysellSignals as $signal) {
+			if ($signal[1] == "buy") {
+				$smaBuy[$ctrBuy++] = $signal;
+			}
+		}*/
+
+		// echo json_encode($smaBuy);
+
+		//Tailor data for the ATR input
+		$ctr = 0;
+		$dataOhlcForATR = [];
+		foreach ((array)$dataOhlc as $ohlc) {
+			$dataOhlcForATR[$ctr][0] = $ohlc[0];	//timestamp
+			$dataOhlcForATR[$ctr][1] = $ohlc[2];	//high
+			$dataOhlcForATR[$ctr][2] = $ohlc[3];	//low
+			$dataOhlcForATR[$ctr][3] = $ohlc[4];	//close
+
+			$ctr = $ctr + 1;
+		}
+
+		// echo json_encode($dataOhlcForATR);
+
+		// Compute the ATR
+		$atr = codesword_atr($dataOhlcForATR);
+
+		// echo json_encode($atr);
+
+		// TODO: Get ATR Stop/Sell Signals
+		//		the stop signal is based on the entry price that we'll get from
+		//		the BB3 Buy Signal
+		// Inputs:
+		//		1. Data OHLC for ATR 	(ts,open,high,low,close)
+		//		2. BB3 Buy Signals 		(ts, buy)
+		//		3. ATR Values 			(ts, atr)
+/*		$smaEntryAtrStopSignals = codesword_smaEntryATRstopTradeDetector($company, 
+										$dataOhlc, $smaBuy, $atr, 2, 4, 
+										$enProfitComputation);*/
+	}
+?>
